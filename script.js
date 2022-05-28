2477fef6ac1b58b99c9080a0750ffddb9c87fc3f
Travis Swift <travis.arthur.swift@gmail.com>
2022-05-28 14:03:15 -0500

    trying to get farhenheit values

--- script.js
+++ script.js
@@ -4,14 +4,14 @@ function GetInfo() {
     var cityName = document.getElementById("cityName");
     cityName.innerHTML = "--"+newName.value+"--";
 
-fetch('https://api.openweathermap.org/data/2.5/forecast?q='+newName.value+'&appid=00c0c03a085f1c057eaf6be2d1ca349e')
+fetch('https://api.openweathermap.org/data/2.5/forecast?q='+ newName.value + '&units=imperial&appid=00c0c03a085f1c057eaf6be2d1ca349e')
 .then(response => response.json())
 .then(data => {
 
     //Getting the min and max values for each day
     for(i = 0; i<5; i++){
         document.getElementById("day" + (i+1) + "Min").innerHTML = "Min: " + Number(data.list[i].main.temp_min - 273.15).toFixed(1)+ "°";
-        //Number(1.3450001).toFixed(2); // 1.35
+   
     }
 
     for(i = 0; i<5; i++){
@@ -31,7 +31,7 @@ fetch('https://api.openweathermap.org/data/2.5/forecast?q='+newName.value+'&appi
 
 })
 
-.catch(err => alert("Something Went Wrong"))
+.catch(err => alert("Something Went Wrong "))
 }
 
 function DefaultScreen(){
@@ -40,11 +40,11 @@ function DefaultScreen(){
 }
 
 
-//Getting and displaying the text for the upcoming five days of the week
+//Getting and displaying the text for the upcoming days of the week
 var d = new Date();
 var weekday = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday",];
 
-//Function to get the correct integer for the index of the days array
+ 
 function CheckDay(day){
     if(day + d.getDay() > 6){
         return day + d.getDay() - 7;
